0b9dae5d326570c7d58e17fcdf3de0d67edd114a
Prakhash <prakhashsiva21@gmaill.com>
2014-03-13 20:02:15 +0000

    FirstCommit

---
+++ app/cache/dev/twig/c7/9a/e095aa7ec2f97d5186c6c82c9184.php
@@ -0,0 +1,254 @@
+<?php
+
+/* LoginLoginBundle:Default:uos.html.twig */
+class __TwigTemplate_c79ae095aa7ec2f97d5186c6c82c9184 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<!DOCTYPE html>
+<html lang=\"en\">
+  <head>
+    <meta charset=\"utf-8\">
+    <title>University of Sumanadasa, Halls Management System</title>
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <meta name=\"description\" content=\"\">
+    <meta name=\"author\" content=\"\">
+
+    <!-- Le styles -->
+    <link href=\"";
+        // line 11
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/css/bootstrap.css"), "html", null, true);
+        echo "\" rel=\"stylesheet\">
+    <style type=\"text/css\">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
+    <link href=\"";
+        // line 18
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/css/bootstrap-responsive.css"), "html", null, true);
+        echo "\" rel=\"stylesheet\">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src=\"../assets/js/html5shiv.js\"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel=\"apple-touch-icon-precomposed\" sizes=\"144x144\" href=\"";
+        // line 26
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/ico/apple-touch-icon-144-precomposed.png"), "html", null, true);
+        echo "\">
+    <link rel=\"apple-touch-icon-precomposed\" sizes=\"114x114\" href=\"";
+        // line 27
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/ico/apple-touch-icon-114-precomposed.png"), "html", null, true);
+        echo "\">
+      <link rel=\"apple-touch-icon-precomposed\" sizes=\"72x72\" href=\"";
+        // line 28
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/ico/apple-touch-icon-72-precomposed.png"), "html", null, true);
+        echo "\">
+                    <link rel=\"apple-touch-icon-precomposed\" href=\"";
+        // line 29
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/ico/apple-touch-icon-57-precomposed.png"), "html", null, true);
+        echo "\">
+                                   <link rel=\"shortcut icon\" href=\"";
+        // line 30
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/ico/favicon.png"), "html", null, true);
+        echo "\">
+  </head>
+
+  <body>
+
+    <div class=\"navbar navbar-inverse navbar-fixed-top\">
+      <div class=\"navbar-inner\">
+        <div class=\"container\">
+          <button type=\"button\" class=\"btn btn-navbar\" data-toggle=\"collapse\" data-target=\".nav-collapse\">
+            <span class=\"icon-bar\"></span>
+            <span class=\"icon-bar\"></span>
+            <span class=\"icon-bar\"></span>
+          </button>
+          <a class=\"brand\" href=\"#\">Halls Management System for University of Sumanadasa</a>
+          <div class=\"nav-collapse collapse\">
+            <ul class=\"nav\">
+   <!--           <li class=\"active\"><a href=\"#\">Home</a></li>
+              <li><a href=\"#about\">About</a></li>
+              <li><a href=\"#contact\">Contact</a></li>
+              <li class=\"dropdown\">
+                <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">Dropdown <b class=\"caret\"></b></a>
+                <ul class=\"dropdown-menu\">
+                  <li><a href=\"#\">Action</a></li>
+                  <li><a href=\"#\">Another action</a></li>
+                  <li><a href=\"#\">Something else here</a></li>
+                  <li class=\"divider\"></li>
+                  <li class=\"nav-header\">Nav header</li>
+                  <li><a href=\"#\">Separated link</a></li>
+                  <li><a href=\"#\">One more separated link</a></li>
+                </ul>
+              </li>
+      -->
+
+            </ul>
+                  
+              
+            <form class=\"navbar-form pull-right\" method =\"POST\" action=\"";
+        // line 66
+        echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("login_login_uos"), "html", null, true);
+        echo "\">
+              <input class=\"span2\" name=\"user_id\" type=\"text\" placeholder=\"User ID\">
+              <input class=\"span2\" name=\"password\" type=\"password\" placeholder=\"Password\">
+              <button type=\"submit\" class=\"btn\">Sign in</button>
+            </form>
+
+
+
+
+
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    <div class=\"container\">
+
+      <!-- Main hero unit for a primary marketing message or call to action -->
+      
+<!--      <img src=\"C:\\wamp\\www\\uos\\src\\Login\\LoginBundle\\Resources\\public\\images\\size,jpg\" alt=\"some_text\">
+--> 
+
+
+<img src=\"C:\\wamp\\www\\uos\\src\\Login\\LoginBundle\\Resources\\public\\images\\size.jpg\" class=\"img-polaroid\">
+<!--
+<img src=\"";
+        // line 91
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/images/size.jpg"), "html", null, true);
+        echo "\" class=\"img-polaroid\">
+
+<!--
+<div class=\"hero-unit\">
+        <h1>Hello, world!</h1>
+        <p>This is a template for a simple marketing or informational website. It includes a large is is a template for a simple marketing or informational website. It includes a large cis is a template for a simple marketing or informational website. It includes a large cis is a template for a simple marketing or informational website. It includes a laris is a template for a simple marketing or informational website. It includes a large cis is a template for a simple marketing or informational website. It includes a large cis is a template for a simple marketing or informational website. It includes a large cis is a template for a simple marketing or informational website. i includes a large ccis is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        
+      </div>
+-->
+      <!-- Example row of columns -->
+      
+      <div class=\"row\">
+        <div class=\"span4\">
+            
+          <h3>About us</h3>
+          
+          <p>us commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          
+        </div>
+        <div class=\"span4\">
+          <h3>Contact us</h3>
+          <p>
+          Brunthavan Murugaiah </br>
+          University of Moratuwa </br>
+          Srilanka
+          </p>
+          
+       </div>
+        <div class=\"span4\">
+          <h3>More details</h3>
+          <p>a porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          
+        </div>
+      </div>
+              
+      <hr>
+
+      <footer>
+        <p>&copy; Augmented Software Solutions 2014</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src=\"";
+        // line 137
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/js/jquery.js"), "html", null, true);
+        echo "\"></script>
+    <script src=\"";
+        // line 138
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/js/bootstrap-transition.js"), "html", null, true);
+        echo "\"></script>
+    <script src=\"";
+        // line 139
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/js/bootstrap-alert.js"), "html", null, true);
+        echo "\"></script>
+    <script src=\"";
+        // line 140
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/js/bootstrap-modal.js"), "html", null, true);
+        echo "\"></script>
+    <script src=\"";
+        // line 141
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/js/bootstrap-dropdown.js"), "html", null, true);
+        echo "\"></script>
+    <script src=\"";
+        // line 142
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/js/bootstrap-scrollspy.js"), "html", null, true);
+        echo "\"></script>
+    <script src=\"";
+        // line 143
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/js/bootstrap-tab.js"), "html", null, true);
+        echo "\"></script>
+    <script src=\"";
+        // line 144
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/js/bootstrap-tooltip.js"), "html", null, true);
+        echo "\"></script>
+    <script src=\"";
+        // line 145
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/js/bootstrap-popover.js"), "html", null, true);
+        echo "\"></script>
+    <script src=\"";
+        // line 146
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/js/bootstrap-button.js"), "html", null, true);
+        echo "\"></script>
+    <script src=\"";
+        // line 147
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/js/bootstrap-collapse.js"), "html", null, true);
+        echo "\"></script>
+    <script src=\"";
+        // line 148
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/js/bootstrap-carousel.js"), "html", null, true);
+        echo "\"></script>
+    <script src=\"";
+        // line 149
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/loginlogin/js/bootstrap-typeahead.js"), "html", null, true);
+        echo "\"></script>
+
+  </body>
+</html>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "LoginLoginBundle:Default:uos.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  232 => 149,  228 => 148,  224 => 147,  220 => 146,  216 => 145,  212 => 144,  208 => 143,  204 => 142,  200 => 141,  196 => 140,  192 => 139,  188 => 138,  184 => 137,  135 => 91,  107 => 66,  68 => 30,  64 => 29,  60 => 28,  56 => 27,  52 => 26,  41 => 18,  31 => 11,  19 => 1,);
+    }
+}
